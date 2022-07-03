daa737171a06d55877104b4d6222b261144cb78b
Vladimir <v.laptev1@g.nsu.ru>
2022-07-03 21:30:45 +0700

    ок

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 /**
@@ -24,30 +24,37 @@ class MainActivity : AppCompatActivity() {
         // Set a click listener on the button to roll the dice when the user taps the button
         rollButton.setOnClickListener { rollDice() }
 
+        // Do a dice roll when the app starts
+        rollDice()
+
     }
 
     /**
      * Roll the dice and update the screen with the result.
      */
     private fun rollDice() {
-        // Create new Dice object with 6 sides and roll it
+        // Create new Dice object with 6 sides and roll the dice
         val dice = Dice(6)
         val diceRoll = dice.roll()
 
-        // Update the screen with the dice roll
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+        // Find the ImageView in the layout
+        val diceImage: ImageView = findViewById(R.id.imageView)
 
-        val luckyNumber = 4
-
-        when (diceRoll) {
-            luckyNumber -> println("You won!")
-            1 -> println("So sorry! You rolled a 1. Try again!")
-            2 -> println("Sadly, you rolled a 2. Try again!")
-            3 -> println("Unfortunately, you rolled a 3. Try again!")
-            5 -> println("Don't cry! You rolled a 5. Try again!")
-            6 -> println("Apologies! You rolled a 6. Try again!")
+        // Determine which drawable resource ID to use based on the dice roll
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
         }
+
+        // Update the ImageView with the correct drawable resource ID
+        diceImage.setImageResource(drawableResource)
+
+        // Update the content description
+        diceImage.contentDescription = diceRoll.toString()
     }
 }
 
